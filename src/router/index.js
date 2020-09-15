f32e3ca1ed51b96b8f7e0b700430228696d87593
silashuereca <silasgranthuereca@gmail.com>
2020-09-15 13:57:18 -0500

    fix: macos not detecting capitalization with netlify build

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import home from '../views/home.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', name: 'Home', component: home },
+  { path: '/', name: 'Home', component: Home },
 ];
 
 const router = new VueRouter({
