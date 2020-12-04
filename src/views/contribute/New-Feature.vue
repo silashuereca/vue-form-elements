44115a68f99d9015deaba0d7faa7527a153e0d8a
silashuereca <silasgranthuereca@gmail.com>
2020-12-03 21:53:21 -0600

    refactor: updated vue cave's email adress

--- src/views/contribute/New-Feature.vue
+++ src/views/contribute/New-Feature.vue
@@ -3,7 +3,7 @@
     <section class="step-container">
       <div class="step-actions">
         <h2>Let's Talk!</h2>
-        <p>Have an idea to make this site better? Just email us here at <span class="text-highlight">vuecave@gmail.com</span> so we can discuss this further into detail. We would love to hear from you! :)</p>
+        <p>Have an idea to make this site better? Just email us here at <span class="text-highlight">vuecave.site@gmail.com</span> so we can discuss this further into detail. We would love to hear from you! :)</p>
       </div>
     </section>
   </div>
